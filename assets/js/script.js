e72d20e046fb941f7ba3beb24bed58c28aff3e84
Ivan <rivanyshka@gmail.com>
2017-10-27 15:56:12 +0300

    first part

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $(window).on('load scroll', function () {
+    var scrolled = $(this).scrollTop();
+    $('#headerVideo').css('transform', 'translate3d(0, ' + (scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
+  });
+});
