bb205b73a5a56d508f51b359b82e60f0afe4c226
Ivan <Ivan@Mac-mini-IvanO.local>
2017-11-01 17:16:54 +0200

    modal and mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
+
   $(window).on('load scroll', function () {
     var scrolled = $(this).scrollTop();
     $('#headerVideo').css('transform', 'translate3d(0, ' + (scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
+  });
+
+  $("body").niceScroll({
+    scrollspeed: 200,
   });
 });
