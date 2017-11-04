ce8aaf063143c6b6fb51fc589db2e74ed99beb26
Ivan <Ivan@Mac-mini-IvanO.local>
2017-11-04 13:58:56 +0200

    with last fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,22 @@ $(document).ready(function () {
     $('#headerVideo').css('transform', 'translate3d(0, ' + (scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
   });
 
-  $("body").niceScroll({
-    scrollspeed: 100,
-    zindex:-1,
-    touchbehavior: true,
-    emulatetouch: true,
-    cursordragontouch
+  // $("body").niceScroll({
+  //   scrollspeed: 100,
+  //   zindex:-1,
+  //   touchbehavior: true,
+  //   emulatetouch: true,
+  //   cursordragontouch: true
+  // });
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    var headerVideo = $('#headerVideo').height();
+    console.log(scroll, headerVideo);
+    if (scroll >= headerVideo) {
+      $("#videoBottom").addClass("fixed");
+    } else{
+      $("#videoBottom").removeClass("fixed");
+    }
   });
 });
